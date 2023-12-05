01b2e05a9b909dfb2669e92c1983e58384a44640
Andreas Seeger <splash@appfros.ch>
2023-12-05 08:34:30 +0100

    implements a bare bone example of what the issue is

--- so-tca-docBasedWithTabs/so_tca_docBasedWithTabsApp.swift
+++ so-tca-docBasedWithTabs/so_tca_docBasedWithTabsApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct so_tca_docBasedWithTabsApp: App {
     var body: some Scene {
         DocumentGroup(newDocument: so_tca_docBasedWithTabsDocument()) { file in
-            ContentView(document: file.$document)
+            AppView(document: file.$document)
         }
     }
 }
